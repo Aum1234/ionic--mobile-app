d1465e4630fee5574764885527be7eee7a2afaa4
Aum1234 <lalida.am.62@ubu.ac.th>
2022-03-13 14:54:56 +0700

    frist

--- src/app/index/index.page.ts
+++ src/app/index/index.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { Router, RouterModule } from '@angular/router';
+import { IonicAuthService } from '../ionic-auth.service';
 
 
 @Component({
@@ -10,17 +12,42 @@ import { Observable } from 'rxjs';
   styleUrls: ['./index.page.scss'],
 })
 export class IndexPage implements OnInit {
+  userDetail: string;
 
   itemCollection: AngularFirestoreCollection<any>;
   item: Observable<any>;
   items: Observable<any[]>;
 
-constructor(private firestore: AngularFirestore) {
+constructor
+(private firestore: AngularFirestore,
+  private router: Router,
+  private ionicAuthService: IonicAuthService) {
   this.itemCollection = firestore.collection<any>('works');
   this.items = this.itemCollection.valueChanges();
  }
 
   ngOnInit() {
+    //authen
+    this.ionicAuthService.userDetails().subscribe(response => {
+      if (response !== null) {
+        this.userDetail = response.email;
+      } else {
+        this.router.navigateByUrl('');
+      }
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  //signout
+  signOut() {
+    this.ionicAuthService.signoutUser()
+      .then(res => {
+        this.router.navigateByUrl('login');
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   remove(id:string){
